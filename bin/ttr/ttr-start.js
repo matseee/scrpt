8fd3594ba34483f9a20ca4c66fca03d5af3157eb
Matthias Fischer <m@tthias.space>
2020-12-18 16:22:25 +0100

    feat(ttr): add end-task function

--- bin/ttr/ttr-start.js
+++ bin/ttr/ttr-start.js
@@ -1,5 +1,15 @@
 #!/usr/bin/env node
+const figlet = require('figlet');
+
 const TimeTracker = require('../../lib/ttr/ttr');
+
+console.log(figlet.textSync('ttr', {
+    font: 'ANSI Shadow',
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+    width: 80,
+    whitespaceBreak: true
+}));
 
 const timeTracker = new TimeTracker();
 timeTracker.start();
