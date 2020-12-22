08619f5c6e60665a455160f40ca482ae304584c6
Matthias Fischer <m@tthias.space>
2020-12-22 19:34:27 +0100

    feat(ttr): fix task end-command

--- bin/ttr/ttr-end.js
+++ bin/ttr/ttr-end.js
@@ -1,16 +1,7 @@
 #!/usr/bin/env node
 const args = require('args');
-const figlet = require('figlet');
 
 const TimeTracker = require('../../lib/ttr/ttr');
-
-console.log(figlet.textSync('ttr', {
-    font: 'ANSI Shadow',
-    horizontalLayout: 'default',
-    verticalLayout: 'default',
-    width: 80,
-    whitespaceBreak: true
-}));
 
 const params = args.parse(process.argv, {
     name: 'ttr end',
@@ -18,7 +9,7 @@ const params = args.parse(process.argv, {
 });
 
 if (!args.sub.length) {
-    args.showHelp();
+    console.log('Please enter a task description ...');
 } else {
     const timeTracker = new TimeTracker();
     timeTracker.end(args.sub[ 0 ]);
--- bin/ttr/ttr-start.js
+++ bin/ttr/ttr-start.js
@@ -1,15 +1,5 @@
 #!/usr/bin/env node
-const figlet = require('figlet');
-
 const TimeTracker = require('../../lib/ttr/ttr');
-
-console.log(figlet.textSync('ttr', {
-    font: 'ANSI Shadow',
-    horizontalLayout: 'default',
-    verticalLayout: 'default',
-    width: 80,
-    whitespaceBreak: true
-}));
 
 const timeTracker = new TimeTracker();
 timeTracker.start();
